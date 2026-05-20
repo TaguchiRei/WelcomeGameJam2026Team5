e1ac0cc5dd675c14518f86d436cd5cd293e82cdd
vahkd33114 <vahkd33114@yahoo.co.jp>
2026-05-20 17:08:40 +0900

    update: PrizeController.cs

--- Assets/Kanda/PrizeController.cs
+++ Assets/Kanda/PrizeController.cs
@@ -2,13 +2,15 @@ using UnityEngine;
 
 public class PrizeController : MonoBehaviour
 {
+    [SerializeField] private string prizeTag = "PrizeTag";
+
     // つかまれている　状態判定
     public bool isKinematic = false;
     // クレーンのtransformを引数で受け取る
     // クレーンの子オブジェクトにする　同期
 
     /// <summary>
-    ///　つかまれたら停止する
+    /// つかまれたら停止する
     /// クレーンのtransformを引数で受け取る
     /// クレーンの子オブジェクトにする　同期
     /// </summary>
@@ -25,5 +27,17 @@ public class PrizeController : MonoBehaviour
     private void PrizeRelease()
     {
         isKinematic = false;
+    }
+
+    /// <summary>
+    /// つかまれたオブジェクトにPrizeTagがついていたら解除する
+    /// </summary>
+    public void PrizeRelease(GameObject grabbedObject)
+    {
+        if (!grabbedObject.CompareTag(prizeTag))
+        {
+            return;
+        }
+        PrizeRelease();
     }
 }
