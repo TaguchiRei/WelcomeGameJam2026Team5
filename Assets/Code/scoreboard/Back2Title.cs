5e6e06d6492504e3d2a17a6b658532991a70c636
gustavmahlerurlicht <gustavmahlerurlicht@gmail.com>
2026-05-21 13:09:12 +0900

    優勝者が決定された後、２秒間なにも操作できないです。 2秒後、SPACE 押したらタイトル画面へもどるのは可能になります。

---
+++ Assets/Code/scoreboard/Back2Title.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Back2Title : MonoBehaviour
+{
+    public float waitTime = 2f;
+    private bool canReturn = false;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        canReturn = false;
+        Invoke("EnableReturn", waitTime);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (canReturn && Input.GetKeyDown(KeyCode.Space)) 
+        {
+            ReturnToTitle();
+        }
+    }
+
+    void EnableReturn() 
+    {
+        canReturn = true;
+    }
+
+    public void ReturnToTitle() 
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("TitleScene");    
+    }
+}
