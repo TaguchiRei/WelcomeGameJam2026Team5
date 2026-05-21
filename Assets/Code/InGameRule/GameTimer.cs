5da4a651c787516a260bdbd61c996e3626206374
reitaghchi <ren.len2426@gmail.com>
2026-05-21 16:23:51 +0900

    [update]リザルト表示をするように、タイマーを機能させる

--- Assets/Code/InGameRule/GameTimer.cs
+++ Assets/Code/InGameRule/GameTimer.cs
@@ -4,10 +4,16 @@ using UnityEngine.UI;
 public class GameTimer : MonoBehaviour
 {
     public bool IsTimeUp => _currentTime <= 0;
+
     /// <summary> 残り時間のカウントダウンを表示するUIテキスト </summary>
     [SerializeField] private Text _remainingTimeText;
+
     /// <summary> ゲーム終了までの残り秒数 </summary>
-    [SerializeField] private float _remainingTime = 30f; 
+    [SerializeField] private float _remainingTime = 30f;
+
+    [SerializeField] private Image _timerImage;
+
+    [SerializeField] private Winner _winner;
 
     /// <summary> ゲームが終了判定済みかどうかを示すフラグ </summary>
     private bool _isTimeUp;
@@ -25,12 +31,21 @@ public class GameTimer : MonoBehaviour
 
         _currentTime -= Time.deltaTime;
 
-        _remainingTimeText.text = "Time : " + (int)_currentTime ;
+        _timerImage.fillAmount = _currentTime / _remainingTime;
+
+        _remainingTimeText.text = "Time : " + (int)_currentTime;
 
         if (_currentTime <= 0)
         {
             _isTimeUp = true;
-            //TODO ここでリザルトを表示するコードを呼び出す
+
+            // スコアを取得してリザルトを表示
+            ScoreTextManager scoreManager = FindFirstObjectByType<ScoreTextManager>();
+            if (scoreManager != null && _winner != null)
+            {
+                _winner.SetScore(scoreManager.CustomerTotalScore, scoreManager.StoreTotalScore);
+                _winner.ShowResult();
+            }
         }
     }
 }
